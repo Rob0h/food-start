b991f0920ac707afe66c4136b58dbabdfd39dcf2
Rob0h <robert.is.oh@gmail.com>
2016-12-06 14:52:45 -0800

    Current state of MVP - Login alerts not working

--- app/navbar/navbar.js
+++ app/navbar/navbar.js
@@ -35,27 +35,3 @@ appNavBar.controller('navBarCtrl', function($scope, $location) {
      return active;
   };
 });
-
-appNavBar.directive('bsActiveLink', ['$location', function ($location) {
-  return {
-    restrict: 'A', //use as attribute 
-    scope: true,
-    replace: false,
-    link: function (scope, elem) {
-        //after the route has changed
-        scope.$on("$routeChangeSuccess", function () {
-            var hrefs = ['/#' + $location.path(),
-                         '#' + $location.path(), //html5: false
-                         $location.path()]; //html5: true
-            angular.forEach(elem.find('a'), function (a) {
-                a = angular.element(a);
-                if (-1 !== hrefs.indexOf(a.attr('href'))) {
-                    a.parent().addClass('active');
-                } else {
-                    a.parent().removeClass('active');   
-                };
-            });     
-        });
-    }
-  }
-}]);
